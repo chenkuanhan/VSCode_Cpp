c32174b744451b3c272d248b6eb9d0d1c3e88f5d
Hank Chen <kuanc1784@gmail.com>
2025-01-30 16:44:21 +0800

    2nd-commit

--- main.cpp
+++ main.cpp
@@ -2,6 +2,6 @@
 using namespace std;
 
 int main() {
-    cout << "Hello, VS Code C++!" << endl;
+    cout << "lab - Using VS Code to upload code to GitHub and observe changes" << endl;
     return 0;
 }
