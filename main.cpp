22c69584be2a088d4b3fb7f48d9b279da297a245
Hank Chen <kuanc1784@gmail.com>
2025-01-30 17:33:39 +0800

    after public setting

--- main.cpp
+++ main.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 using namespace std;
 int main() {
-    cout << "lab - Using VS Code to upload code to GitHub and observe changes" << endl;
-    cout << "lab - add one line on  line-6" << endl;
-    cout << "clone test" << endl;
+    string name = "Hank";
+    cout<<name;
     return 0;
 
 }
