961233c2a6fb36b93dbcd0efe0c3bdcfd249c267
Hank Chen <kuanc1784@gmail.com>
2025-01-30 17:11:03 +0800

    after git clone 1

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@ using namespace std;
 int main() {
     cout << "lab - Using VS Code to upload code to GitHub and observe changes" << endl;
     cout << "lab - add one line on  line-6" << endl;
+    cout << "clone test" << endl;
     return 0;
 
 }
