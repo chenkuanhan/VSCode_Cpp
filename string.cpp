89d29bf9c06d1788ef5165a3aaad43297aa197ec
Han Han <guanhanchen6@gmail.com>
2025-01-31 19:49:02 +0800

    type casting1

--- string.cpp
+++ string.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
-int main(){
-    const char* string1 = "Hello";
+#include <cstdlib>  // 用於 std::stoi 和 std::stof
+using namespace std;
 
-    std::cout << *(string1+1)  << std::endl;
+int main() {
+    string a = "10";
+    string b = "3.0";
+    int c = stoi(a)+stoi(b);
+    cout << c << endl;  
     return 0;
-    
-
 }
