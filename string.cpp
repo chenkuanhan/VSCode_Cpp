1a86881feb10da695def843bdaaabd23299f9d1b
Han Han <guanhanchen6@gmail.com>
2025-01-31 18:04:45 +0800

    string course 4

--- string.cpp
+++ string.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 int main(){
-    std::string str = "Hello World";
-    for (int i = 0; i < str.length(); i++){
-        std::cout << str[i] << std::endl;
-
-    }
-        
+    std::string string1 = "Hello";
+    std::string *ptr= &string1;
+    std::cout << *ptr  << std::endl;
+    return 0;
+    
 
 }
