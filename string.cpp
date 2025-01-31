bda197df41c08247330bd36fef1749037569c165
Han Han <guanhanchen6@gmail.com>
2025-01-31 23:11:07 +0800

    characteristic2

--- string.cpp
+++ string.cpp
@@ -6,15 +6,16 @@ public:
     string name;
     int age;
 
-    Person(string n, int a) : name(n), age(a) {}
+    Person(string n, int a) {
+    name = n;
+    age = a;
+}
 
-    friend ostream& operator<<(ostream& os, const Person& p) {
-        os << "Name: " << p.name << ", Age: " << p.age;
-        return os;
-    }
+
+
 };
 
 int main() {
     Person p("Alice", 30);
-    cout << p << endl; // ✅ 透過 operator<< 輸出
+    cout << p.name << " " <<p.age << endl; // 
 }
