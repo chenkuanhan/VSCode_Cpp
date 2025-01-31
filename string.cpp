2746f94ba889dedf98b16f0084b9a5914693ce9c
Han Han <guanhanchen6@gmail.com>
2025-01-31 22:56:56 +0800

    c++ characteristic

--- string.cpp
+++ string.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
-#include <cstdlib>  // 用於 std::stoi 和 std::stof
 using namespace std;
 
+class Person {
+public:
+    string name;
+    int age;
+
+    Person(string n, int a) : name(n), age(a) {}
+
+    friend ostream& operator<<(ostream& os, const Person& p) {
+        os << "Name: " << p.name << ", Age: " << p.age;
+        return os;
+    }
+};
+
 int main() {
-    char a = 10;
-    char b = 4.0;
-    double c = a+b;
-    cout << c << endl;  
-    return 0;
+    Person p("Alice", 30);
+    cout << p << endl; // ✅ 透過 operator<< 輸出
 }
